c3ff82616c870174fd7d61ecbbfaf7ef208a41e7
Sam Nordlinger <snordlinger@gmail.com>
2020-05-23 00:04:36 -0600

    Format 8XXX operations

--- python/opcode_8xxx_operations.py
+++ python/opcode_8xxx_operations.py
@@ -1,14 +1,18 @@
 def assignment_8XX0(x, y, carry):
     return y, carry
 
+
 def bitwise_or_8XX1(x, y, carry):
     return x | y, carry
+
 
 def bitwise_and_8XX2(x, y, carry):
     return x & y, carry
 
+
 def xor_8XX3(x, y, carry):
     return x ^ y, carry
+
 
 def addition_8XX4(x, y, carry):
     reg_sum = x + y
@@ -16,15 +20,18 @@ def addition_8XX4(x, y, carry):
         return reg_sum % 256, 1
     return reg_sum, 0
 
+
 def sub_x_y_8XX5(x, y, carry):
     diff = x - y
     if diff < 0:
         return 256 + diff, 1
     return diff, 0
 
+
 def shift_right_8XX6(x, y, carry):
     least_significant_bit = x % 2
     return x >> 1, least_significant_bit
+
 
 def sub_y_x_8XX7(x, y, carry):
     diff = y - x
@@ -32,9 +39,11 @@ def sub_y_x_8XX7(x, y, carry):
         return 256 + diff, 1
     return diff, 0
 
+
 def shift_left_8XXE(x, y, carry):
     most_significant_bit = x >> 7
     return (x << 1) % 256, most_significant_bit
+
 
 operation_table = [
     assignment_8XX0,
@@ -46,6 +55,7 @@ operation_table = [
     shift_right_8XX6,
     sub_y_x_8XX7,
 ]
+
 
 def apply(opcode, x, y, carry):
     last_hex_digit = opcode & 0x000F
