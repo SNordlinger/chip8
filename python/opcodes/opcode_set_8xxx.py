8f397e476e31f5add54c137d6c1baab9243c99ee
Sam Nordlinger <snordlinger@gmail.com>
2020-05-30 23:02:37 -0600

    Add comment and fix styles in 8xxx opcodes

--- python/opcodes/opcode_set_8xxx.py
+++ python/opcodes/opcode_set_8xxx.py
@@ -3,17 +3,23 @@ from util import get_opcode_digits
 
 
 class OpcodeSet8xxx(OpcodeSet):
+    """
+    8XY0 - 8XYE
+    Each of the many 8xxx opcodes sets the VX register
+    (and sometimes the carry flag) based on the state
+    of the VX and VY registers
+    """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.operation_table = [
-            self.assignment_8XX0,
-            self.bitwise_or_8XX1,
-            self.bitwise_and_8XX2,
-            self.xor_8XX3,
-            self.addition_8XX4,
-            self.sub_x_y_8XX5,
-            self.shift_right_8XX6,
-            self.sub_y_x_8XX7,
+            self.assignment_8xx0,
+            self.bitwise_or_8xx1,
+            self.bitwise_and_8xx2,
+            self.xor_8xx3,
+            self.addition_8xx4,
+            self.sub_x_y_8xx5,
+            self.shift_right_8xx6,
+            self.sub_y_x_8xx7,
         ]
 
     def execute(self, opcode):
@@ -26,19 +32,19 @@ class OpcodeSet8xxx(OpcodeSet):
             op = self.operation_table[last_hex_digit]
         op(x_reg, x_value, y_value)
 
-    def assignment_8XX0(self, x_reg, x_value, y_value):
+    def assignment_8xx0(self, x_reg, x_value, y_value):
         self.registers.v[x_reg] = y_value
 
-    def bitwise_or_8XX1(self, x_reg, x_value, y_value):
+    def bitwise_or_8xx1(self, x_reg, x_value, y_value):
         self.registers.v[x_reg] = x_value | y_value
 
-    def bitwise_and_8XX2(self, x_reg, x_value, y_value):
+    def bitwise_and_8xx2(self, x_reg, x_value, y_value):
         self.registers.v[x_reg] = x_value & y_value
 
-    def xor_8XX3(self, x_reg, x_value, y_value):
+    def xor_8xx3(self, x_reg, x_value, y_value):
         self.registers.v[x_reg] = x_value ^ y_value
 
-    def addition_8XX4(self, x_reg, x_value, y_value):
+    def addition_8xx4(self, x_reg, x_value, y_value):
         reg_sum = x_value + y_value
         if reg_sum > 255:
             self.registers.v[x_reg] = reg_sum % 256
@@ -47,7 +53,7 @@ class OpcodeSet8xxx(OpcodeSet):
             self.registers.v[x_reg] = reg_sum
             self.registers.v[0xF] = 0
 
-    def sub_x_y_8XX5(self, x_reg, x_value, y_value):
+    def sub_x_y_8xx5(self, x_reg, x_value, y_value):
         diff = x_value - y_value
         if diff < 0:
             self.registers.v[x_reg] = 256 + diff
@@ -56,12 +62,12 @@ class OpcodeSet8xxx(OpcodeSet):
             self.registers.v[x_reg] = diff
             self.registers.v[0xF] = 0
 
-    def shift_right_8XX6(self, x_reg, x_value, y_value):
+    def shift_right_8xx6(self, x_reg, x_value, y_value):
         least_significant_bit = x_value % 2
         self.registers.v[x_reg] = x_value >> 1
         self.registers.v[0xF] = least_significant_bit
 
-    def sub_y_x_8XX7(self, x_reg, x_value, y_value):
+    def sub_y_x_8xx7(self, x_reg, x_value, y_value):
         diff = y_value - x_value
         if diff < 0:
             self.registers.v[x_reg] = 256 + diff
@@ -70,9 +76,7 @@ class OpcodeSet8xxx(OpcodeSet):
             self.registers.v[x_reg] = diff
             self.registers.v[0xF] = 0
 
-    def shift_left_8XXE(self, x_reg, x_value, y_value):
+    def shift_left_8xxE(self, x_reg, x_value, y_value):
         most_significant_bit = x_value >> 7
         self.registers.v[x_reg] = (x_value << 1) % 256
         self.registers.v[0xF] = most_significant_bit
-
-
